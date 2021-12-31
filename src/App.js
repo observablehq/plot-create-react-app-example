f161a645c2fd660ad1ff6322b71f355bfb5e580d
Mike Bostock <mbostock@gmail.com>
2021-12-31 09:00:05 -0800

    add Plot example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import * as Plot from "@observablehq/plot";
 import './App.css';
+import {useEffect, useRef} from 'react';
 
 function App() {
+  const headerRef = useRef();
+
+  useEffect(() => {
+    const chart = Plot.plot({
+      style: {
+        background: "transparent"
+      },
+      marks: [
+        Plot.ruleY([0]),
+        Plot.line({length: 200}, {x: (_, i) => i, y: Math.random})
+      ]
+    });
+    headerRef.current.append(chart);
+    return () => chart.remove();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" ref={headerRef}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
