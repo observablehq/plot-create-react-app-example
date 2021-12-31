ef1337a0a1340894d2ab7df1f1cfff7499319fab
Mike Bostock <mbostock@gmail.com>
2021-12-31 09:05:21 -0800

    use real data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import * as Plot from "@observablehq/plot";
+import * as d3 from "d3";
 import './App.css';
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 function App() {
   const headerRef = useRef();
+  const [data, setData] = useState();
 
   useEffect(() => {
+    d3.csv("/gistemp.csv", d3.autoType).then(setData);
+  }, []);
+
+  useEffect(() => {
+    if (data === undefined) return;
     const chart = Plot.plot({
       style: {
         background: "transparent"
       },
+      y: {
+        grid: true
+      },
+      color: {
+        type: "diverging",
+        scheme: "burd"
+      },
       marks: [
         Plot.ruleY([0]),
-        Plot.line({length: 200}, {x: (_, i) => i, y: Math.random})
+        Plot.dot(data, {x: "Date", y: "Anomaly", stroke: "Anomaly"})
       ]
     });
     headerRef.current.append(chart);
     return () => chart.remove();
-  }, []);
+  }, [data]);
 
   return (
     <div className="App">
